6c109f23fd05d70441ebc37c167f26bb691dcc2e
Sebastian Luy <sluy@competitive-energy.com>
2021-12-19 09:48:01 +0100

    Add camera

--- core/src/com/sebluy/mygame/MyGame.java
+++ core/src/com/sebluy/mygame/MyGame.java
@@ -2,19 +2,23 @@ package com.sebluy.mygame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 import java.util.*;
 
 public class MyGame extends ApplicationAdapter {
+	static final float CAMERA_SPEED = 100.0f;
 	ShapeRenderer shapeRenderer;
 	Map<Integer, Person> people;
 	Map<Integer, Person> team1;
 	Map<Integer, Person> team2;
 	Map<Integer, Bullet> bullets;
 	Person pickedUp;
+	OrthographicCamera camera;
 	float time = 0;
 	float lastShot = -1;
 
@@ -29,6 +33,10 @@ public class MyGame extends ApplicationAdapter {
 
 		int width = Gdx.graphics.getWidth();
 		int height = Gdx.graphics.getHeight();
+
+		camera = new OrthographicCamera(width, height);
+		camera.position.set((float)width / 2, (float)height / 2, 0);
+		camera.update();
 
 		Gdx.input.setInputProcessor(new InputAdapter() {
 			@Override
@@ -51,8 +59,8 @@ public class MyGame extends ApplicationAdapter {
 		});
 
 			for (int x = width / 10 ; x < width; x += width / 10) {
-				Person p1 = new Person(this, x, height / 10);
-				Person p2 = new Person(this, x, height * 9 / 10);
+				Person p1 = new Person(this, x, (double)height / 10);
+				Person p2 = new Person(this, x, (double)height * 9 / 10);
 				team1.put(p1.id, p1);
 				team2.put(p2.id, p2);
 			}
@@ -75,14 +83,7 @@ public class MyGame extends ApplicationAdapter {
 		}
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		shootIfNecessary();
-
-		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+	private void updateIfShot() {
 		ArrayList<Bullet> removeB = new ArrayList<>();
 		ArrayList<Person> removeP = new ArrayList<>();
 		for (Bullet b : bullets.values()) {
@@ -102,6 +103,35 @@ public class MyGame extends ApplicationAdapter {
 			team1.remove(p.id);
 			team2.remove(p.id);
 		}
+	}
+
+	private void handleCameraMovement() {
+		float distance = CAMERA_SPEED * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+			camera.translate(0, distance);
+		} else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+			camera.translate(0, -distance);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+			camera.translate(-distance, 0);
+		} else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+			camera.translate(distance, 0);
+		}
+	}
+
+	@Override
+	public void render () {
+		handleCameraMovement();
+		shootIfNecessary();
+		updateIfShot();
+
+		Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
 		for (Bullet b : bullets.values()) {
 			b.render();
 		}
